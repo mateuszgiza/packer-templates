8e965be13298fb9e1915853e17509b3d1aa9f0c8
Damian Szymański <damian@assembla.com>
2019-11-24 08:31:42 +0100

    Override system python version

--- cookbooks/travis_ci_sardonyx/attributes/default.rb
+++ cookbooks/travis_ci_sardonyx/attributes/default.rb
@@ -62,6 +62,7 @@ override['travis_build_environment']['nodejs_versions'] = %w[
 ]
 override['travis_build_environment']['nodejs_default'] = '12.13.1'
 
+override['travis_build_environment']['system_python']['pythons'] = %w[3.8]
 pythons = %w[
   3.6.9
   3.7.5
